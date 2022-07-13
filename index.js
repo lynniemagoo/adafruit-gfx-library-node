530e1a0a87932639da2b224f767d93065512d584
lynniemagoo <lynniemagoo@yahoo.com>
2022-07-12 19:02:42 -0500

    Add Adafruit TTF converted fonts - Note that TomThumb is the non-extended version.
    Fonts are loaded dynamically using a proxy.

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ const Display = {Display_Base, Adafruit_SPITFT};
 const Mixins = {Mixin_I2C_Display, ...Mixin_SPI_Display_Module, Mixin_RBG565_Display_Render, Mixin_RGB565_GFXcanvas16_Render};
 const Utils = {...UtilsModule};
 
+const UTILS_LOCATION = LIB_LOCATION + "fonts/";
+const Fonts = require(FONTS_LOCATION + "FontProxy.js");
+
 module.exports = {
-    Canvas, Core, Display, Mixins, Utils
+    Canvas, Core, Display, Fonts, Mixins, Utils
 }
