68e7f6137111769e2bae752e836a54ce48fe1b96
lynniemagoo <lynniemagoo@yahoo.com>
2022-07-14 19:28:13 -0500

    Add I2C Defaults and ColorUtils_RGB565

--- index.js
+++ index.js
@@ -15,19 +15,20 @@ const Display_Base = require(DISPLAY_LOCATION + "Display_Base.js");
 const Adafruit_SPITFT = require(DISPLAY_LOCATION + "Adafruit_SPITFT.js");
 
 const MIXINS_LOCATION = LIB_LOCATION + "mixins/";
-const Mixin_I2C_Display = require(MIXINS_LOCATION + "Mixin_I2C_Display.js");
+const Mixin_I2C_Display_Module = require(MIXINS_LOCATION + "Mixin_I2C_Display.js");
 const Mixin_SPI_Display_Module = require(MIXINS_LOCATION + "Mixin_SPI_Display.js");
 const Mixin_RBG565_Display_Render = require(MIXINS_LOCATION + "Mixin_RBG565_Display_Render.js");
 const Mixin_RGB565_GFXcanvas16_Render = require(MIXINS_LOCATION + "Mixin_RGB565_GFXcanvas16_Render.js");
 
 const UTILS_LOCATION = LIB_LOCATION + "utils/";
-const UtilsModule = require(UTILS_LOCATION + "CommonUtils.js");
+const CommonUtilsModule = require(UTILS_LOCATION + "CommonUtils.js");
+const ColorUtils_RGB565 = require(UTILS_LOCATION + "ColorUtils_RGB565.js");
 
 const Canvas = {...GFXcanvas1Module, GFXcanvas8, GFXcanvas16, GFXcanvas16_RGB565};
 const Core = {Adafruit_GFX};
 const Display = {Display_Base, Adafruit_SPITFT};
-const Mixins = {Mixin_I2C_Display, ...Mixin_SPI_Display_Module, Mixin_RBG565_Display_Render, Mixin_RGB565_GFXcanvas16_Render};
-const Utils = {...UtilsModule};
+const Mixins = {...Mixin_I2C_Display_Module, ...Mixin_SPI_Display_Module, Mixin_RBG565_Display_Render, Mixin_RGB565_GFXcanvas16_Render};
+const Utils = {...CommonUtilsModule, ColorUtils_RGB565 };
 
 const FONTS_LOCATION = LIB_LOCATION + "fonts/";
 const Fonts = require(FONTS_LOCATION + "FontProxy.js");
