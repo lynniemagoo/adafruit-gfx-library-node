70efcab313d643d5a7621cac4901b6a396b4b8aa
lynniemagoo <lynniemagoo@yahoo.com>
2022-08-27 22:40:48 -0500

    Add Adafruit_GrayOLED to Display submodule.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const Adafruit_GFX = require(CORE_LOCATION + "Adafruit_GFX.js");
 const DISPLAY_LOCATION = LIB_LOCATION + "display/";
 const Display_Base = require(DISPLAY_LOCATION + "Display_Base.js");
 const Adafruit_SPITFT = require(DISPLAY_LOCATION + "Adafruit_SPITFT.js");
+const Adafruit_GrayOLED = require(DISPLAY_LOCATION + "Adafruit_GrayOLED.js");
 
 const MIXINS_LOCATION = LIB_LOCATION + "mixins/";
 const Mixin_I2C_Display_Module = require(MIXINS_LOCATION + "Mixin_I2C_Display.js");
@@ -23,7 +24,7 @@ const CommonUtilsModule = require(UTILS_LOCATION + "CommonUtils.js");
 
 const Canvas = {...GFXcanvas1Module, GFXcanvas8, GFXcanvas16};
 const Core = {Adafruit_GFX};
-const Display = {Display_Base, Adafruit_SPITFT};
+const Display = {Display_Base, Adafruit_SPITFT, Adafruit_GrayOLED};
 const Mixins = {...Mixin_I2C_Display_Module, ...Mixin_SPI_Display_Module, Mixin_RBG565_Display_Render};
 const Utils = {...CommonUtilsModule};
 
